8c2daf1574508dff64954fb25af308b37f23dbb7
Paul Chavarria <pjchavarria@gmail.com>
2015-02-07 13:32:50 -0800

    Initial Commit

---
+++ LinkAlbum/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LinkAlbum
+//
+//  Created by Paul Chavarria Podoliako on 2/7/15.
+//  Copyright (c) 2015 AnyTap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
